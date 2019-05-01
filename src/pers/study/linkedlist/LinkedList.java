2910690ba0e49d0b28e21c7cf96f7d5ef151d22d
Just-EP <lovelive7185@outlook.com>
2019-05-01 17:48:40 +0800

    Initial commit

---
+++ src/pers/study/linkedlist/LinkedList.java
@@ -0,0 +1,29 @@
+package pers.study.linkedlist;
+
+/**
+ * @ClassName LinkedList
+ * @Author JustEP
+ * @Version 1.0
+ * @Description 实现链表功能
+ * @CreateTime 2019/05/01 07:35:00
+ */
+public class LinkedList {
+    /**
+     * @Description 链表头结点
+     */
+    private Node head;
+    /**
+     * @Description 当前结点
+     */
+    private Node current;
+    /**
+     * @Description 向链表中添加元素
+     * @Param [node]
+     * @Return void
+     * @Author JustEP
+     * @Date 2019/5/1 7:41
+     */
+    public void addNode(Node node){
+
+    }
+}
